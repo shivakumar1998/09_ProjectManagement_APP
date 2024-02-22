cc45029e90c88ad97306e617b87fae85f5ee685f
P.shivakumar <60265650+shivakumar1998@users.noreply.github.com>
2024-02-22 12:20:13 +0530

    session 156

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -10,7 +10,7 @@ export default function ProjectsSidebar({ onStartAddProject, projects }) {
       <div>
         <Button onClick={onStartAddProject}>+ Add Project</Button>
       </div>
-      <ul className="mt-8 ">
+      <ul className="mt-8">
         {projects.map((project) => (
           <li key={project.id}>
             <button className="w-full text-left px-2 py-1 rounded-sm my-1 text-stone-400 hover:text-stone-200 hover:bg-stone-800">
